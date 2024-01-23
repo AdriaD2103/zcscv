042713e0d45d0465620b2c142eb471b48d7372c2
AdriaD2103 <73031896+AdriaD2103@users.noreply.github.com>
2024-01-23 18:32:32 +0100

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,19 +1,39 @@
+"use client";
 import React from "react";
 import HeroContent from "../sub/HeroContent";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Hero = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
-    <div className="relative flex flex-col h-full w-full" id="about-me">
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      variants={containerVariants}
+      className="relative flex flex-col h-full w-full"
+      id="about-me"
+    >
       <video
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+        className="rotate-180 absolute top-[-455px] h-min w-full left-0 z-[1] object-cover"
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
       <HeroContent />
-    </div>
+    </motion.div>
   );
 };
 
